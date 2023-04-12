83b248454e551cb118f57afd17b3ec31495fa44e
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-12 22:11:03 +0300

    task day 1

---
+++ src/TaskNumber1.java
@@ -0,0 +1,8 @@
+
+//Напишите программу, которая выводит на экран строку  "Hello world!"
+
+public class TaskNumber1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
