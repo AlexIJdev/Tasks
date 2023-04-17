66f3e61ab5d3d63c165809ab9197a6b29cf7ab1d
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-17 21:40:46 +0300

    task day 2

--- src/TaskNumber2.java
+++ src/TaskNumber2.java
@@ -1,2 +1,14 @@
-public class TaskNumber2 {
+
+// Закомментируйте ненужные строки так, чтобы в выводе получилось:
+// "// - Это комментарий) Это не сложно"
+
+public class TaskNumber2{
+    public static void main(String[] args){
+//        System.out.println("#  - Это комментарий) Стоп, это не тот язык");
+          System.out.println("// - Это комментарий) Это не сложно");
+//        System.out.println("// - Это не комментарий) Это не сложно");
+//        System.out.println("// - Это комментарий) Это сложно");
+//        System.out.println("// - Это не комментарий) Это не сложно");
+//        System.out.println("можно даже попробовать многострочный комментарий");
+    }
 }
