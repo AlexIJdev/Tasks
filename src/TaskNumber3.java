7d305f3515f4fe6a31f39a88a0ac3c7ec8a342ed
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-19 23:13:24 +0300

    task day 3

---
+++ src/TaskNumber3.java
@@ -0,0 +1,10 @@
+
+// Напишите программу, которая выведет на экран "Hello Programming!" (без кавычек)
+// на 2 строках, по слову на каждой (см. пример вывода)
+
+public class TaskNumber3 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Programming!");
+    }
+}
