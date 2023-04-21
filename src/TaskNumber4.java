dcfd75a005f426c10041ced782c1e3e318d48e7d
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-21 23:35:40 +0300

    task day 4

---
+++ src/TaskNumber4.java
@@ -0,0 +1,18 @@
+
+//  Закомментируйте ненужные строки так, что бы в выводе получилось:
+//  "Я прекрасно понимаю разницу между System.out.println()
+//  и System.out.print(). А ты?"
+
+public class TaskNumber4 {
+    public static void main(String[] args) {
+//      System.out.println("Я прекрасно понимаю разницу ");
+        System.out.print("Я прекрасно понимаю разницу ");
+        System.out.println("между System.out.println()");
+//      System.out.print("между System.out.println()");
+//      System.out.println("и System.out.print(). ");
+        System.out.print("и System.out.print(). ");
+        System.out.println("А ты?");
+//      System.out.print("А ты?");
+    }
+}
+
