7e273d30f8f89146893dd00d61772ccb632519d5
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-23 23:40:27 +0300

    task day 6

---
+++ src/TaskNumber6.java
@@ -0,0 +1,14 @@
+
+//  Дано целое число n. Выведите следующее за ним четное число.
+//  При решении этой задачи нельзя использовать условную инструкцию if и циклы.
+//  Подсказка: Подумайте, что будет оставаться в остатке при делении на 2 у чётных и у нечётных чисел.
+
+import java.util.Scanner;
+
+public class TaskNumber6 {
+    public static void main(String[] args){
+        Scanner line = new Scanner(System.in);
+        int number = line.nextInt();
+        System.out.println(((number - 1) % 2) + ((number) % 2) + ((number + 1) % 2) + number);
+    }
+}
