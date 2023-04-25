99ca7c44d4f4cec243afa0bd96471720d472531b
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-25 23:35:27 +0300

    task day 8

---
+++ src/TaskNumber8.java
@@ -0,0 +1,12 @@
+
+// Дано трехзначное число. Найдите сумму его цифр.
+
+import java.util.Scanner;
+
+public class TaskNumber8 {
+    public static void main(String[] args){
+        Scanner line = new Scanner(System.in);
+        int randomNumber = line.nextInt();
+        System.out.println((randomNumber / 100) + ((randomNumber % 100) / 10) + (randomNumber % 10));
+    }
+}
