39012ed1515ef3b2f2b0eb2842c9a6a827a41992
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-04-28 18:41:52 +0300

    task day 11

---
+++ src/TaskNumber11.java
@@ -0,0 +1,14 @@
+
+// Напишите программу, которая считывает три целых числа и выводит их произведение.
+
+import java.util.Scanner;
+
+public class TaskNumber11 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int numberOne = sc.nextInt();
+        int numberTwo = sc.nextInt();
+        int numberThree = sc.nextInt();
+        System.out.println(numberOne*numberTwo*numberThree);
+    }
+}
