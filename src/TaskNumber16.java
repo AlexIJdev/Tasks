057bbebd010c8d4d91c2984e63405fb5d0794e6b
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-05-05 23:52:06 +0300

    task day 16

---
+++ src/TaskNumber16.java
@@ -0,0 +1,12 @@
+
+// Дано произвольное положительное натуральное число. Найдите его последнюю цифру.
+
+import java.util.Scanner;
+
+public class TaskNumber16 {
+    public static void main(String[] args) {
+        Scanner line = new Scanner(System.in);
+        int randomNumber = line.nextInt();
+        System.out.println(randomNumber % 10);
+    }
+}
