ee68c658573fead04c55c9e7575c0417dc32b160
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-05-08 23:43:44 +0300

    task day 19

---
+++ src/TaskNumber19.java
@@ -0,0 +1,17 @@
+
+// Улитка ползёт по вертикальному шесту высотой h метров, поднимаясь за день на a метров,
+// а за ночь спускаясь на b метров. На какой день улитка доползёт до вершины шеста?
+// Программа получает на вход натуральные числа h, a, b. Гарантируется, что a>b.
+// Программа должна вывести одно натуральное число.
+
+import java.util.Scanner;
+
+public class TaskNumber19 {
+    public static void main(String[] args) {
+        Scanner lineOne = new Scanner(System.in);
+        int height = lineOne.nextInt();
+        int heightCrawlUp = lineOne.nextInt();
+        int heightCrawlDown = lineOne.nextInt();
+        System.out.println(((height - heightCrawlDown) - 1) / (heightCrawlUp - heightCrawlDown) + 1);
+    }
+}
