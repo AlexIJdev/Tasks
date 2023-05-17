6ac3eb9779852a17d5110f4274ccd6b9fc96a131
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-05-17 23:41:19 +0300

    task day 25

---
+++ src/TaskNumber25.java
@@ -0,0 +1,36 @@
+
+// ВАЛЛ-И очень любит читать. В его книге n страниц и каждый день он читает по s страниц.
+// Составьте программу, которая считает, сколько страниц останется ВАЛЛ-И прочитать через 5 дней.
+//
+// Входные данные:
+// Вводятся два целых неотрицательных числа:
+// n - количество страниц в книге
+// s  - количество страниц, которые прочитывает ВАЛЛ-И каждый день
+//
+// Выходные данные:
+// выведите на экран ответ на задачу: сколько страниц останется прочитать ВАЛЛ-И через 5 дней.
+//
+// Sample Input:
+// 100
+// 10
+//
+// Sample Output:
+// 50
+//
+// Sample Input:
+// 800
+// 20
+//
+// Sample Output:
+// 700
+
+import java.util.Scanner;
+
+public class TaskNumber25 {
+    public static void main(String[] args) {
+        Scanner line = new Scanner(System.in);
+        int pagesOfTheBook = line.nextInt();
+        int pagesReadOnDay = line.nextInt();
+        System.out.println(pagesOfTheBook - pagesReadOnDay * 5);
+    }
+}
