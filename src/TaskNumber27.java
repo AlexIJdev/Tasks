8a695c2336d3c9c403eb5cbcd3e4222c6800eddb
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-05-20 23:48:02 +0300

    task day 27

---
+++ src/TaskNumber27.java
@@ -0,0 +1,32 @@
+//
+// ВАЛЛ-И встретил n детей и хочет разделить между ними k яблок так, чтобы каждому досталось поровну.
+// Не делящийся остаток он собирается отнести ЕВЕ. Сколько яблок достанется каждому ребенку?
+// Сколько яблок получит ЕВА? Результат выведите в одной строке через пробел в указанном порядке.
+//
+// Входные данные: два натуральных числа:
+// n - количество детей
+// k - количество яблок
+//
+// Выходные данные: выведите ответ на задачу:
+// Sample Input:
+//        2
+//        6
+// Sample Output:
+//        3 0
+//
+// Sample Input:
+//        4
+//        25
+// Sample Output:
+//        6 1
+
+import java.util.Scanner;
+
+public class TaskNumber27 {
+    public static void main(String[] args) {
+        Scanner line = new Scanner(System.in);
+        int numbersOfChildren = line.nextInt();
+        int numbersOfApple = line.nextInt();
+        System.out.println((numbersOfApple - (numbersOfApple % numbersOfChildren)) / numbersOfChildren + " " + (numbersOfApple % numbersOfChildren));
+    }
+}
