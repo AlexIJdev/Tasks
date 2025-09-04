86b0c3f4992d088fa452f12cfce7269cdeca2bb0
Alexandr Borovik <www.sanya.boriy@mail.ru>
2025-09-04 22:46:42 +0300

    Task day 4

---
+++ src/tasks/task1/Number1.java
@@ -0,0 +1,24 @@
+package tasks.task1;
+
+/*
+    Коллекции: уникальные элементы сохраняя порядок
+    Проблема: дан список строк. Вернуть новый список, который содержит уникальные элементы в порядке их первого появления.
+    Вход: ["apple", "banana", "apple", "orange", "banana", "kiwi"]
+    Выход: ["apple", "banana", "orange", "kiwi"]
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class Number1 {
+
+    public static void main(String[] args) {
+        System.out.println(getUniqueList(List.of("apple", "banana", "apple", "orange", "banana", "kiwi")));
+    }
+
+    public static List<String> getUniqueList(List<String> inputlist) {
+        // собираем элементы в LinkedHashSet, который сохраняет порядок вставки и обеспечивает уникальность
+        return new ArrayList<>(new LinkedHashSet<>(inputlist));
+    }
+}
